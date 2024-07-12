fb6e7734720f17be0eb6cdf7d2bdb3470fa44075
Sudarshan <k.sudarshan2904@gmail.com>
2024-07-12 23:08:31 +0530

    ch lit code

--- model.py
+++ model.py
@@ -51,3 +51,14 @@ def final_result(query):
     qa_result = qa_bot()
     response = qa_result({'query': query})
     return response
+
+#chainlit code
+@cl.on_chat_start
+async def start():
+    chain = qa_bot()
+    msg = cl.Message(content="Starting the bot...")
+    await msg.send()
+    msg.content = "Hi, Welcome to Medical Bot. What is your query?"
+    await msg.update()
+
+    cl.user_session.set("chain", chain)
