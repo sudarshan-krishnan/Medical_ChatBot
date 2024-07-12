5cb782d4a67a8e075eab6d6a1aa9cbbfb7534d31
Sudarshan <k.sudarshan2904@gmail.com>
2024-07-12 22:39:30 +0530

    Retrieval QA Chain

--- model.py
+++ model.py
@@ -14,3 +14,12 @@ def set_custom_prompt():
                             input_variables=['context', 'question'])
     return prompt
 
+#Retrieval QA Chain
+def retrieval_qa_chain(llm, prompt, db):
+    qa_chain = RetrievalQA.from_chain_type(llm=llm,
+                                       chain_type='stuff',
+                                       retriever=db.as_retriever(search_kwargs={'k': 2}),
+                                       return_source_documents=True,
+                                       chain_type_kwargs={'prompt': prompt}
+                                       )
+    return qa_chain
