f5a18aacf1a274490e67a074a582eef7c28c205a
Sudarshan <k.sudarshan2904@gmail.com>
2024-07-12 22:38:01 +0530

    the imports

---
+++ model.py
@@ -0,0 +1,9 @@
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.prompts import PromptTemplate
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.llms import CTransformers
+from langchain.chains import RetrievalQA
+import chainlit as cl
+
+
